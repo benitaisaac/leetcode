29df661192452e9761615ba2cd2886c22bcc539a
Benita <benita.isaac@gmail.com>
2025-02-02 11:19:16 -0800

    find largest

--- beginner-level/find_largest_element.js
+++ beginner-level/find_largest_element.js
@@ -1,3 +1,15 @@
 // Write a method that takes a list of integers as input and returns the smallest element in the list
 // Example Input: [3,5,2,9,1]
 // Output: 1 
+
+function findLargest(arr){
+    let largest = arr[0];
+    for (let i=0; i < arr.length; i++){
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
+
+console.log(findLargest([4,2,7,1]));
